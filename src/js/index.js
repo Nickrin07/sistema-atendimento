7a5e95884bb2eaf5074d76d83693cf997c6eaf68
Nickollas Matheus <nickollas.tatter@gmail.com>
2023-08-21 22:44:47 -0400

    add funcoes e eventos

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
 import "./libs"
+const inputNome =
+ document.querySelector('#input-nome')
+ const selectPrioridade =
+ document.querySelector('#select-prioridade')
+const btnIncluir = 
+document.querySelector('#btn-incluir')
+const ulLista = 
+document.querySelector('#ul-lista')
+const displayNome = 
+document.querySelector('#display-nome')
+const btnChamar = 
+document.querySelector('#btn-chamar')
+const listaDeAtendimento = new Array()
+
+function incluir(){
+    listaDeAtendimento.push(inputNome.value)
+    listar()
+}
+
+function listar(){
+    for (let nome of listaDeAtendimento){
+        console.log(nome)
+    }
+}
+//eventos
+btnIncluir.onclick = incluir
